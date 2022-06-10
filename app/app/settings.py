0d4890d684bd8f774929b522f38052fb4638efbe
Kenan <kenan23@gmail.com>
2022-06-10 19:02:52 +0300

    convert to postgresql.

--- app/app/settings.py
+++ app/app/settings.py
@@ -84,8 +84,11 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'sqlite3.db',
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': os.environ.get('DB_HOST'),
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASS'),
     }
 }
 #
