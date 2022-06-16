c9e5a54ad2dc9300638e4820d9fa7526e6f23877
Kenan <kenan23@gmail.com>
2022-06-16 17:42:51 +0300

    update request

--- app/core/views.py
+++ app/core/views.py
@@ -14,27 +14,27 @@ from drf_spectacular.utils import (
 from rest_framework import serializers
 
 
-class Geeks(object):
+class Compare(object):
     def __init__(self, image1, image2):
         self.image1 = image2
         self.image2 = image2
 
 
 # create a serializer
-class GeeksSerializer(serializers.Serializer):
+class CompareSerializer(serializers.Serializer):
     # initialize fields
     image1 = serializers.FileField()
     image2 = serializers.FileField()
 
 @extend_schema_view(
     get=extend_schema(description='get desc', responses={}),
-    post=extend_schema(description='post desc', request=GeeksSerializer, responses=OpenApiTypes.UUID),
+    post=extend_schema(description='post desc', request=CompareSerializer, responses=OpenApiTypes.UUID),
 )
 @api_view(['POST'])
 def health_check(request):
     """Returns successful response."""
-    image1 = request.query_params.get('image1')
-    image2 = request.query_params.get('image2')
+    image1 = request.data.get('image1')
+    image2 = request.data.get('image2')
     print(image1)
     print(image2)
     return Response({'healthy': True})
