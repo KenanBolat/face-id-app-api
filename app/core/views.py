650b73e62af4cd2972eae25aaf8b3957d09caec8
Kenan Bolat <kenan23@gmail.com>
2022-06-17 01:43:11 +0300

    local test in saved files.

--- app/core/views.py
+++ app/core/views.py
@@ -1,6 +1,9 @@
 """
 Core views for app.
 """
+import io
+import os
+import uuid
 from io import BytesIO
 
 from deepface import DeepFace
@@ -12,8 +15,20 @@ from drf_spectacular.utils import (
     OpenApiTypes,
 )
 from faceid.prediction import read_image, preprocess
+from django.core.files.storage import default_storage
+from django.core.files.base import ContentFile
+from django.conf import settings
+
 
 from rest_framework import serializers
+
+
+def faceid_image_file_path(filename):
+    """Generate file path for new faceid image."""
+    ext = os.path.splitext(filename)[1]
+    filename = f'{uuid.uuid4()}{ext}'
+
+    return os.path.join('uploads', 'faceid', filename)
 
 
 class Compare(object):
@@ -25,8 +40,8 @@ class Compare(object):
 # create a serializer
 class CompareSerializer(serializers.Serializer):
     # initialize fields
-    image1 = serializers.FileField()
-    image2 = serializers.FileField()
+    image1 = serializers.ImageField()
+    image2 = serializers.ImageField()
 
 
 @extend_schema_view(
@@ -36,10 +51,36 @@ class CompareSerializer(serializers.Serializer):
 def compare_view(request):
     """Gets two separate image and  compares them."""
     print(request.FILES)
-    image1 = preprocess(read_image(BytesIO(request.FILES["image1"].read())))
-    image2 = preprocess(read_image(BytesIO(request.FILES["image2"].read())))
+
+    # c = Compare.objects.all()
+    # s = CompareSerializer(c, many=True)
+    # print("==" * 5)
+    # img1 = "/vol/web/media/uploads/faceid/" + s.data[0]['image'].split('/')[-1]
+    # imgRead1 = read_image(img1)
+
+    print("==" * 5)
+
+    path = default_storage.save('tmp/img1.png', ContentFile(request.FILES["image1"].read()))
+    tmp_file1 = os.path.join(settings.MEDIA_ROOT, path)
+    path2 = default_storage.save('tmp/img2.png', ContentFile(request.FILES["image2"].read()))
+    tmp_file2 = os.path.join(settings.MEDIA_ROOT, path2)
+    print(tmp_file1)
+    imgRead1 = read_image(tmp_file1)
+    imgRead2 = read_image(tmp_file2)
+    preprocess1 = preprocess(imgRead1)
+    preprocess2 = preprocess(imgRead2)
     model_name = 'VGG-Face'
-    result = DeepFace.verify(img1_path=image1, img2_path=image2, model_name=model_name)
+    result = DeepFace.verify(img1_path=preprocess1, img2_path=preprocess2, model_name=model_name)
+
+    #
+    # print(img1)
+    #
+    # with io.BytesIO() as output:
+    #     img1.save(output, format="PNG")
+    #     contents = output.getvalue()
+    # image1 = preprocess(read_image(img1))
+    # image2 = preprocess(read_image(BytesIO(request.FILES["image2"].read())))
+    # result = DeepFace.verify(img1_path=image1, img2_path=image2, model_name=model_name)
 
     return Response(result)
 
