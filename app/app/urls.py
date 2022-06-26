c3d459727b7e8c7cd606ccfecae0487aa8105ae9
Kenan Bolat <kenan23@gmail.com>
2022-06-26 11:44:33 +0300

    debug toolbar

--- app/app/urls.py
+++ app/app/urls.py
@@ -37,7 +37,5 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(
-        settings.MEDIA_URL,
-        document_root=settings.MEDIA_ROOT,
-    )
+    import debug_toolbar
+    urlpatterns += [path(include('__debug__/', debug_toolbar.urls)), ]
